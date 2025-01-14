ad10ca4981fa9ef13e68401890fe203a426c9cb6
lagr-web <manglerbarelige@gmail.com>
2025-01-14 14:23:55 +0100

    types included

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,20 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 import { getAllData } from "@/lib/data";
+import { Product } from "@/lib/type/types";
+
 
 export default async function Home() {
 
   const data = await getAllData();
-
 
   return (
     <>
 
     {data &&
 
-       data.products.map( (item:any)=>(
-        
+       data.products.map( (item:Product)=>(
+
         <div>
           <Link
            href={{
